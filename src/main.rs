a5bb8a658786786359ab94c4d48afbd5c452fbee
alstocras <qzrnw@proton.me>
2026-08-11 15:40:47 +0000

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-// imports!!!
-use iced;
+/* // imports!!!
 
 // main function (like c++ i think)
-fn main() -> () {
+fn main() {
+    // printer
     println!("Hello, world!");
+} */
 
+use std::io;
+
+fn main() {
+    println!("pls guess");
+    let mut guess: String = String::new();
+    io::stdin().read_line(&mut guess).expect("fail");
+    println!("you guess: {guess}")
 }
