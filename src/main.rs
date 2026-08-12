eaacd7c2ce2f3850f6c7fb9a64513a4c73c2aaad
alstocras <qzrnw@proton.me>
2026-08-12 01:46:39 +0000

    learning more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
-/* // imports!!!
+// imports
+use rand::*;
+use std::*;
 
-// main function (like c++ i think)
+// main function
 fn main() {
-    // printer
-    println!("Hello, world!");
-} */
-
-use std::io;
-
-fn main() {
+    // prompt
     println!("pls guess");
+
+    // get random num
+    let rNum = rand::rng().random_range(1..=100);
+
+    // empty string to store input
     let mut guess: String = String::new();
+
+    // read line and give it to the mutable reference guess
     io::stdin().read_line(&mut guess).expect("fail");
-    println!("you guess: {guess}")
+
+    // confirm guess
+    println!("you guess: {guess}");
+
+    // scoping
+    {
+        // convert to string
+        let guess: u32 = guess.trim().parse().expect("invalid");
+
+        // check cases
+        match guess.cmp(&rNum) {
+            cmp::Ordering::Less => println!("smaller"),
+            cmp::Ordering::Greater => println!("greater"),
+            cmp::Ordering::Equal => println!("yay you got it!!"),
+        }
+    }
 }
