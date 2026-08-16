00dab0db2135ff020f30c20c0d6d67898d78eb3a
alstocras <qzrnw@proton.me>
2026-08-16 15:19:50 +0000

    preparation

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,25 @@
+// imports
+use iced::*;
+use std::*;
 
+// timeline struct
+#[derive(Default)]
+struct Timeline {
+    line: collections::HashMap,
+}
+
+// timeline functions
+impl Timeline {
+    // updates everything
+    fn update(timeline: &mut Timeline, mess: Message) {}
+}
+
+// main runner
+fn main() -> Result {
+    run(update, view);
+}
+
+// kinds of interactions
+enum Message {
+    Quit,
+}
