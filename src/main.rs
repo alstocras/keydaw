083726cdb692ffc454aff99ea5748ac3385e9568
alstocras <qzrnw@proton.me>
2026-08-16 15:59:00 +0000

    im SO lost

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,31 @@ use iced::*;
 use std::*;
 
 // timeline struct
-#[derive(Default)]
-struct Timeline {
-    line: collections::HashMap,
+#[derive(Debug, Default)]
+struct Timeline<T> {
+    line: collections::HashMap<u64, f64>,
 }
 
 // timeline functions
-impl Timeline {
+impl<T> Timeline<T> {
     // updates everything
-    fn update(timeline: &mut Timeline, mess: Message) {}
+    fn update(timeline: &mut Timeline<T>, mess: Message) {
+        match mess {
+            Message::Quit => exit::<Task<T>>(),
+        };
+    }
+    fn view(timeline: &Timeline<T>) -> Element<'_, Message> {
+        widget::button("quit").on_press(Message::Quit).into()
+    }
+}
+
+// kinds of interactions
+#[derive(Debug, Clone)]
+enum Message {
+    Quit,
 }
 
 // main runner
 fn main() -> Result {
-    run(update, view);
-}
-
-// kinds of interactions
-enum Message {
-    Quit,
+    run(Timeline::<Task<T>>::update, Timeline::view)
 }
