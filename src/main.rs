be31addf48d0bffbaea622a41e0fe43ef24a7444
alstocras <qzrnw@proton.me>
2026-08-17 01:27:23 +0000

    feature: the app has a quit button now :) (#2)
    * some classes and stuff
    
    * a bunch of base setup
    
    * i have NO idea what is going on
    
    * no cookie?
    
    * restart kinda (the iced docs suck)
    
    * im scratching it again
    
    * preparation
    
    * im SO lost
    
    * get rid of all the compiler recommended stuff
    
    * WE HAVE A WORKING QUIT BUTTON

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,33 @@
-// placeholder to stop rust-analyzer panicking
-fn main() {
-    println!("this is keydaw");
+// imports
+use iced::*;
+use std::*;
+
+// timeline struct
+#[derive(Debug, Default)]
+struct Timeline {
+    line: collections::HashMap<u64, f64>,
+}
+
+// timeline functions
+impl Timeline {
+    // updates everything
+    fn update(timeline: &mut Timeline, mess: Message) -> Task<Message> {
+        match mess {
+            Message::Quit => process::exit(0),
+        };
+    }
+    fn view(timeline: &Timeline) -> Element<'_, Message> {
+        widget::button("quit").on_press(Message::Quit).into()
+    }
+}
+
+// kinds of interactions
+#[derive(Debug, Clone)]
+enum Message {
+    Quit,
+}
+
+// main runner
+fn main() -> Result {
+    run(Timeline::update, Timeline::view)
 }
