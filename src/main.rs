02f9cfb7720ead4cb3959be6463c7c5e08e0fa26
alstocras <qzrnw@proton.me>
2026-08-17 01:56:39 +0000

    incrementer placeholder

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::*;
 // timeline struct
 #[derive(Debug, Default)]
 struct Timeline {
+    // timeline map <time, pitch>
     line: collections::HashMap<u64, f64>,
 }
 
@@ -14,16 +15,28 @@ impl Timeline {
     fn update(timeline: &mut Timeline, mess: Message) -> Task<Message> {
         match mess {
             Message::Quit => process::exit(0),
+            Message::IncTime => {
+                timeline.line.insert(0, 0.0);
+                println!("{:?}", timeline.line)
+            }
         };
+        return Task::none();
     }
+
+    // shows the app
     fn view(timeline: &Timeline) -> Element<'_, Message> {
-        widget::button("quit").on_press(Message::Quit).into()
+        widget::row![
+            widget::button("quit").on_press(Message::Quit),
+            widget::button("increment time").on_press(Message::IncTime)
+        ]
+        .into()
     }
 }
 
 // kinds of interactions
 #[derive(Debug, Clone)]
 enum Message {
+    IncTime,
     Quit,
 }
 
